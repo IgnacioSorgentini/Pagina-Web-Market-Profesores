146510c3124137f075784f4a011f9f6567b61b01
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-09-24 15:29:19 -0300

    Estructura general del menu

---
+++ src/Componentes/Menu.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Menu () {
+    return(
+        <div className="menu">
+            <div className="titulo">
+                <h1>Este es el titulo</h1>
+            </div>
+            <div className="botones">
+                <button>Boton1</button>
+                <button>Boton2</button>
+                <button>Boton3</button>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
