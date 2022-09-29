c592c28bf4bb939096d9802183fbb6bd29c0ed02
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-09-28 23:19:41 -0300

    Insertados los botones para realizar filtros (Sin el filtrado de por sí)

--- src/Pantallas/Contratar-clase.jsx
+++ src/Pantallas/Contratar-clase.jsx
@@ -8,8 +8,57 @@ function ContratarCLase () {
 
     return(
         <div className="contenedor-mis-clases">
-            <div className="titulo-pantalla">
-                <h3>Clases disponbles</h3>
+            <div className="cabecera-pantalla">
+                <div className="titulo-pantalla">
+                    <h3>Clases disponibles</h3>
+                </div>
+                <div className="filtro-clases">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm">MATERIA</button>
+                        <button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="visually-hidden">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><button class="dropdown-item" type="button">Biologia</button></li>
+                            <li><button class="dropdown-item" type="button">Matematica</button></li>
+                            <li><button class="dropdown-item" type="button">Electronica</button></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm">CLASE</button>
+                        <button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="visually-hidden">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><button class="dropdown-item" type="button">Individual</button></li>
+                            <li><button class="dropdown-item" type="button">Grupal</button></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm">FRECUENCIA</button>
+                        <button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="visually-hidden">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><button class="dropdown-item" type="button">Unica</button></li>
+                            <li><button class="dropdown-item" type="button">Semanal</button></li>
+                            <li><button class="dropdown-item" type="button">Mensual</button></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group estrellas">
+                        <button type="button" class="btn btn-sm">CALIFICACION</button>
+                        <button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="visually-hidden">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon></button></li>
+                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon></button></li>
+                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon></button></li>
+                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon></button></li>
+                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon></button></li>
+                       </ul>
+                    </div>
+                </div>
             </div>
             <div className="lista-clases">
                 <ClaseDisponible Nombre='Biologia' Descripcion='Clase de biologia nivel intermedio' Profesor='Fernanda Gimenez' Dia='Viernes' Horario='7:45am a 11:45am'/>
