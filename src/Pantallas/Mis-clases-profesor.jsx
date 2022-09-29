5b53b5598599bf79aeff310f490246b1dff2dbcd
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-09-29 11:53:49 -0300

    Insertando boton crear clase en pantalla mis clases profesor

--- src/Pantallas/Mis-clases-profesor.jsx
+++ src/Pantallas/Mis-clases-profesor.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ClaseContratada from "../Componentes/ClaseContratada";
 import ClaseCreada from "../Componentes/ClaseCreada";
+import '../Hojas-de-estilo/Mis-clases-profesor.css';
 
 function MisClasesProfesor () {
     return(
         <div className="contenedor-mis-clases">
-            <div className="titulo-pantalla">
-                <h3>Mis clases</h3>
+            <div className="cabecera-pantalla-profesor">
+                <div className="titulo-pantalla">
+                    <h3>Mis clases</h3>
+                </div>
+                <div className="boton-crear-clase">
+                    <button>CREAR CLASE</button>
+                </div>
             </div>
             <div className="lista-clases">
                 <ClaseCreada Nombre="Matematica A" Descripcion="Clase de matematica nivel avanzado" Materia="Matematica" Dia="Miercoles" Horario="6am a 8am" Frecuencia="Semanal" />
