a912ab19c2b5c647cf0ea6042ddbb9bfa2cec6f8
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-09-30 12:42:06 -0300

    Armando componente y creando archivo css

--- src/Componentes/PopupWindowEditar.jsx
+++ src/Componentes/PopupWindowEditar.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 
-function PopupWindowEditar (props) {
-    return(props.trigger) ? (
+function PopupEditarClase(props) {
+    return (props.trigger) ? (
         <div className="popup">
-            <div className="popup-inner">
+            <div className="popup-inner nueva-clase">
                 <div className="caja-btn-popup">
                     <button className="close-btn" onClick={() => props.setTrigger(false)}><ion-icon name="close-outline"></ion-icon></button>
                 </div>
-                <div className="titulo-ventana-comentario">Escribe un comentario</div>
-                <div className="contenedor-texto-ventana">
-                    <input type="text" className="texto-ventana" placeholder="Comentario..." maxLength={100} />
+                <div className="contenedor-titulo-ventana-nueva-clase">
+                    <input type="text" className="titulo-ventana-nueva-clase" placeholder="Titulo de la clase..." />
                 </div>
-                <div className="contenedor-btn-ventana-comentario">
-                    <button>ENVIAR</button>
+                <div className="contenedor-texto-ventana">
+                    <input type="text" className="texto-ventana" placeholder="Descripcion..." maxLength={100} />
+                </div>
+                <div className="contenedor-texto-ventana">
+                    <input type="text" className="texto-ventana" placeholder="Precio..." maxLength={100} />
+                </div>
+                <div className="contenedor-texto-ventana">
+                    <input type="text" className="texto-ventana" placeholder="Materia..." maxLength={100} />
+                </div>
+                <div className="contenedor-texto-ventana">
+                    <input type="text" className="texto-ventana" placeholder="Dias de cursada..." maxLength={100} />
+                </div>
+                <div className="contenedor-texto-ventana">
+                    <input type="text" className="texto-ventana" placeholder="Frecuencia..." maxLength={100} />
+                </div>
+                <div className="contenedor-btn-ventana-nueva-clase">
+                    <button onClick={() => props.setTrigger(false)}>CREAR</button>
                 </div>
             </div>
         </div>
-    ) : "" ;
+    ) : "";
 }
+
+export default PopupEditarClase;
