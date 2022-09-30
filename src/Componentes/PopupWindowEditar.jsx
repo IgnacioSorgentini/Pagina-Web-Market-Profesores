e32821092c191a0664307796276dcc1123d79cbe
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-09-30 12:36:36 -0300

    Arrancando con los popups de ClaseProfesor

---
+++ src/Componentes/PopupWindowEditar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function PopupWindowEditar (props) {
+    return(props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <div className="caja-btn-popup">
+                    <button className="close-btn" onClick={() => props.setTrigger(false)}><ion-icon name="close-outline"></ion-icon></button>
+                </div>
+                <div className="titulo-ventana-comentario">Escribe un comentario</div>
+                <div className="contenedor-texto-ventana">
+                    <input type="text" className="texto-ventana" placeholder="Comentario..." maxLength={100} />
+                </div>
+                <div className="contenedor-btn-ventana-comentario">
+                    <button>ENVIAR</button>
+                </div>
+            </div>
+        </div>
+    ) : "" ;
+}
