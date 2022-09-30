df841da4d19f41ca7a2759f02eda7941ac0d237d
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-09-30 10:56:47 -0300

    Se modificó el componente ClaseContratada

--- src/Componentes/ClaseContratada.jsx
+++ src/Componentes/ClaseContratada.jsx
@@ -5,7 +5,7 @@ import '../Hojas-de-estilo/ClaseGeneral.css';
 import '../Hojas-de-estilo/ClaseContratada.css';
 import {Link} from 'react-router-dom';
 
-function ClaseContratada({Nombre, Descripcion, Profesor, Dia, Horario}) {
+function ClaseContratada({Nombre, Descripcion, Materia, Profesor, Horario, Tipo, Frecuencia, Calificacion}) {
 
     const [buttonPopupComment, setButtonPopupComment] = useState(false);
 
@@ -18,9 +18,12 @@ function ClaseContratada({Nombre, Descripcion, Profesor, Dia, Horario}) {
                 <h6>{Descripcion}</h6>
             </div>
             <div className="detalles-clase">
+                <div className="materia-clase"><ion-icon name="book-outline"></ion-icon><h6>{Materia}</h6></div>
                 <div className="profesor-clase"><ion-icon name="person-outline"></ion-icon><h6>{Profesor}</h6></div>
-                <div className="dia-clase"><ion-icon name="calendar-outline"></ion-icon><h6>{Dia}</h6></div>
                 <div className="horario-clase-cont"><ion-icon name="time-outline"></ion-icon><h6>{Horario}</h6></div>
+                <div className="tipo-clase"><ion-icon name="people-outline"></ion-icon><h6>{Tipo}</h6></div>
+                <div className="frecuencia-clase"><ion-icon name="barbell-outline"></ion-icon><h6>{Frecuencia}</h6></div>
+                <div className="calificacion-clase"><ion-icon name="star-outline"></ion-icon><h6>{Calificacion}</h6></div>
             </div>
             <div className="caja-comentarios-clase">
                 <div className="caja-boton-comentar">
