67e83d70a266135b701bfd2c0eb38b09e5f4a836
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-11-04 08:40:22 -0300

    Cambiando los filtros de la pantalla Contratar Clase

--- src/Pantallas/Contratar-clase.jsx
+++ src/Pantallas/Contratar-clase.jsx
@@ -4,8 +4,39 @@ import { useState } from "react";
 import '../Hojas-de-estilo/Contratar-clase.css';
 import ClaseDisponible from '../Componentes/ClaseDisponible';
 import MenuAlumno from "../Componentes/Menu/MenuAlumno";
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
 
 function ContratarCLase () {
+
+        const [materia, setMateria] = React.useState('');
+      
+        const handleChangeMateria = (event) => {
+          setMateria(event.target.value);
+        };
+
+        const [clase, setClase] = React.useState('');
+      
+        const handleChangeClase = (event) => {
+          setClase(event.target.value);
+        };
+
+        const [frecuencia, setFrecuencia] = React.useState('');
+      
+        const handleChangeFrecuencia = (event) => {
+          setFrecuencia(event.target.value);
+        };
+
+        const [calificacion, setCalificacion] = React.useState('');
+      
+        const handleChangeCalificacion = (event) => {
+          setCalificacion(event.target.value);
+        };
 
     return(
         <div className="ContratarClase">
@@ -16,51 +47,75 @@ function ContratarCLase () {
                     <h3>Clases disponibles</h3>
                 </div>
                 <div className="filtro-clases">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm">MATERIA</button>
-                        <button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="visually-hidden">Toggle Dropdown</span>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><button class="dropdown-item" type="button">Biologia</button></li>
-                            <li><button class="dropdown-item" type="button">Matematica</button></li>
-                            <li><button class="dropdown-item" type="button">Electronica</button></li>
-                        </ul>
-                    </div>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm">CLASE</button>
-                        <button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="visually-hidden">Toggle Dropdown</span>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><button class="dropdown-item" type="button">Individual</button></li>
-                            <li><button class="dropdown-item" type="button">Grupal</button></li>
-                        </ul>
-                    </div>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm">FRECUENCIA</button>
-                        <button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="visually-hidden">Toggle Dropdown</span>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><button class="dropdown-item" type="button">Unica</button></li>
-                            <li><button class="dropdown-item" type="button">Semanal</button></li>
-                            <li><button class="dropdown-item" type="button">Mensual</button></li>
-                        </ul>
-                    </div>
-                    <div class="btn-group estrellas">
-                        <button type="button" class="btn btn-sm">CALIFICACION</button>
-                        <button type="button" class="btn btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="visually-hidden">Toggle Dropdown</span>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon></button></li>
-                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon></button></li>
-                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon></button></li>
-                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon></button></li>
-                            <li><button class="dropdown-item" type="button"><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon></button></li>
-                       </ul>
-                    </div>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" style={{backgroundColor:"#5B4FA6", borderRadius:"5px"}}>
+                        <InputLabel id="demo-select-small" style={{color:"white"}}>Materia</InputLabel>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={materia}
+                                label="Age"
+                                onChange={handleChangeMateria}
+                            >
+                                <MenuItem value="">
+                                    <em>Todo</em>
+                                </MenuItem>
+                                <MenuItem value={10}>Matematica</MenuItem>
+                                <MenuItem value={20}>Fisica</MenuItem>
+                                <MenuItem value={30}>Electronica</MenuItem>
+                            </Select>
+                    </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" style={{backgroundColor:"#5B4FA6", borderRadius:"5px"}}>
+                        <InputLabel id="demo-select-small" style={{color:"white"}}>Clase</InputLabel>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={clase}
+                                label="Age"
+                                onChange={handleChangeClase}
+                            >
+                                <MenuItem value="">
+                                    <em>Todo</em>
+                                </MenuItem>
+                                <MenuItem value={10}>Individual</MenuItem>
+                                <MenuItem value={20}>Grupal</MenuItem>
+                            </Select>
+                    </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" style={{backgroundColor:"#5B4FA6", borderRadius:"5px"}}>
+                        <InputLabel id="demo-select-small" style={{color:"white"}}>Frecuencia</InputLabel>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={frecuencia}
+                                label="Age"
+                                onChange={handleChangeFrecuencia}
+                            >
+                                <MenuItem value="">
+                                    <em>Todo</em>
+                                </MenuItem>
+                                <MenuItem value={10}>Unica vez</MenuItem>
+                                <MenuItem value={20}>Mensual</MenuItem>
+                                <MenuItem value={30}>Semanal</MenuItem>
+                            </Select>
+                    </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small" style={{backgroundColor:"#5B4FA6", borderRadius:"5px"}}>
+                        <InputLabel id="demo-select-small" style={{color:"white"}}>Calificacion</InputLabel>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={calificacion}
+                                label="Age"
+                                onChange={handleChangeCalificacion}
+                            >
+                                <MenuItem value="">
+                                    <em>Todo</em>
+                                </MenuItem>
+                                <MenuItem value={10}><Rating name="read-only" value={1} readOnly /></MenuItem>
+                                <MenuItem value={20}><Rating name="read-only" value={2} readOnly /></MenuItem>
+                                <MenuItem value={30}><Rating name="read-only" value={3} readOnly /></MenuItem>
+                                <MenuItem value={40}><Rating name="read-only" value={4} readOnly /></MenuItem>
+                                <MenuItem value={50}><Rating name="read-only" value={5} readOnly /></MenuItem>
+                            </Select>
+                    </FormControl>
                 </div>
             </div>
             <div className="lista-clases">
