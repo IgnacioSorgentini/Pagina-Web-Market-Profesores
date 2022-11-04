43c9c5499210fbc478f4c1626b77456c11c8c68b
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-11-04 15:44:38 -0300

    Mejorando el componente Comentario Alumno

--- src/Componentes/ComentarioAlumno.jsx
+++ src/Componentes/ComentarioAlumno.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import '../Hojas-de-estilo/ComentarioAlumno.css';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 
 function Comentario({Nombre, Comentario}) {
+    
+    function stringToColor(string) {
+        let hash = 0;
+        let i;
+      
+        /* eslint-disable no-bitwise */
+        for (i = 0; i < string.length; i += 1) {
+          hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+      
+        let color = '#';
+      
+        for (i = 0; i < 3; i += 1) {
+          const value = (hash >> (i * 8)) & 0xff;
+          color += `00${value.toString(16)}`.slice(-2);
+        }
+        /* eslint-enable no-bitwise */
+      
+        return color;
+      }
+      
+      function stringAvatar(name) {
+        return {
+          sx: {
+            bgcolor: stringToColor(name),
+          },
+          children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+        };
+      }
+    
+    
     return(
         <div className="contenedor-comentario">
             <div className="nombre-comentador">
-                <h4>{Nombre}</h4>
+                <div className="avatar-comentador">
+                <Avatar {...stringAvatar(`${Nombre}`)} sx={{ width: 34, height: 34 }} />
+                </div>
+                <div style={{display:"flex", alignItems:"center", paddingTop:"5px"}}>
+                    <h4 style={{color:"white"}}>{Nombre}</h4>
+                </div>
             </div>
             <div className="texto-comentario">
                 <p>{Comentario}</p>
--- src/Pantallas/Comentarios-alumno.jsx
+++ src/Pantallas/Comentarios-alumno.jsx
@@ -12,10 +12,10 @@ function ComentariosAlumno () {
                 <h3>Comentarios</h3>
             </div>
             <div className="lista-comentarios">
-                <ComentarioAlumno Nombre='Ignacio Sorgentini' Comentario='Bla Bla Bla Bla' />
-                <ComentarioAlumno Nombre='Ignacio Sorgentini' Comentario='Bla Bla Bla Bla' />
-                <ComentarioAlumno Nombre='Ignacio Sorgentini' Comentario='Bla Bla Bla Bla' />
-                <ComentarioAlumno Nombre='Ignacio Sorgentini' Comentario='Bla Bla Bla Bla' />
+                <ComentarioAlumno Nombre='Ignacio Sorgentini' Comentario= 'f you are still targeting browsers like IE11 and lower .. well, my heart goes out to you. The below solution I came up with 5 years ago will still work for you. Also, email me if you want a job that doesn' />
+                <ComentarioAlumno Nombre='Pedro Severi' Comentario='Alto bajon.. Pero exponerlo a jugar muy al límite es un riesgo enorme para él, por no decir que el rendimiento no va a ser óptimo para el equipo..' />
+                <ComentarioAlumno Nombre='Mirko Basar' Comentario='Los diálogos de estos tipos son siempre tan absurdos e incoherentes y no en el buen sentido...no esa absurdez graciosa si no solo absurdo y punto' />
+                <ComentarioAlumno Nombre='Ignacio Sorgentini' Comentario='Buenas como estan? Saludos a Alkemy’s team, necesito saber si se puede iniciar a trabajar sabiendo algo de javascript, rubi, c#, c++, java, no tengo experiencia en trabajar pero vi algo de esos lenguajes y todo es similar, si me enseñan a aplicar lo que se o si se puede aprender a donore y servirles para luego asumir un puesto de trabajo, me interesaria, tambien curse 2 años de ingeniería en sistemas y me dedico al servicio tecnico' />
                 <ComentarioAlumno Nombre='Ignacio Sorgentini' Comentario='Bla Bla Bla Bla' />
 
             </div>
