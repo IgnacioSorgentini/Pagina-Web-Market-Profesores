1ca30ec5dbd3e3c3325533767325ad6af95746d6
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-11-08 08:34:06 -0300

    Agregando boton para las notificaciones del alumno

--- src/Componentes/Menu/MenuAlumno.jsx
+++ src/Componentes/Menu/MenuAlumno.jsx
@@ -3,8 +3,32 @@ import { Link } from "react-router-dom";
 import './Menu.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+
 
 function MenuAlumno(){
+    
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleClickNotificaciones = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleCloseNotificaciones = () => {
+      setAnchorEl(null);
+    };
+    const openNotificaciones = Boolean(anchorEl);
+    const id = openNotificaciones ? 'simple-popover' : undefined;
+    
+
+    const [count, setCount] = React.useState(0);
+    const [invisible, setInvisible] = React.useState(false);
+    const handleBadgeVisibility = () => {
+        setInvisible(!invisible);
+    };
+
+
     return(
         <div className="menu">
             <div className="titulo-menu">
@@ -20,6 +44,19 @@ function MenuAlumno(){
                 <Link to="/" style={{textDecoration:"none"}}>
                     <Button variant="outlined" style={{color:"#E6E6E6", borderColor:"#E6E6E6", marginLeft:"30px"}}>Cerrar sesion</Button>
                 </Link>
+                <Button style={{color:"#E6E6E6"}} onClick={handleClickNotificaciones}><Badge badgeContent={count} color="secondary" max={99} showZero><NotificationsIcon/></Badge></Button>
+                <Popover
+                    id={id}
+                    open={openNotificaciones}
+                    anchorEl={anchorEl}
+                    onClose={handleCloseNotificaciones}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                >
+                    <Typography sx={{ p: 2 }}>Acá van las notificaciones</Typography>
+                </Popover>
             </div>
         </div>
     )
