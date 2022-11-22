59bc430415edcce2105f46c5e9bc2e21c4195067
peseveri <pedroseveri@gmail.com>
2022-11-21 21:39:28 -0300

    integracion clases disponibles

--- src/Pantallas/Mis-clases-alumno.jsx
+++ src/Pantallas/Mis-clases-alumno.jsx
@@ -4,7 +4,15 @@ import ClaseContratada from "../Componentes/ClaseContratada";
 import MenuAlumno from '../Componentes/Menu/MenuAlumno';
 
 function MisClasesAlumno ({id}) {
-    console.log(id)
+    const clases = [<ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>,
+    <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>,
+    <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>,
+    <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>,
+    <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>]
+
+    
+    
+
     return(
         <div className="misClasesAlumno">
             <MenuAlumno />
@@ -13,11 +21,12 @@ function MisClasesAlumno ({id}) {
                 <h3 style={{color:"#334756"}}>Mis clases</h3>
             </div>
             <div className="lista-clases">
-                <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>
-                <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>
-                <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>
-                <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>
-                <ClaseContratada Nombre='Logaritmos' Descripcion='Introduccion a los logaritmos' Materia="Matematica" Profesor='Jose Lopez' Horario='1 hora' Tipo="Grupal" Frecuencia="Semanal" Calificacion="4"/>
+                {
+                    clases.map((clase) =>{
+                        return(<ClaseContratada Nombre={clase.props.Nombre} Descripcion={clase.props.Descripcion} Materia={clase.props.Materia} Profesor={clase.props.Profesor} Horario={clase.props.Horario} Tipo={clase.props.Tipo} Frecuencia={clase.props.Frecuencia} Calificacion={clase.props.Calificacion}/>)
+                    })
+                }
+               
             </div>
         </div>
         </div>
