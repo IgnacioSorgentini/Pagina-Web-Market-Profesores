29d6286ad80230b34277da30a0e4c2f53a34bd14
peseveri <pedroseveri@gmail.com>
2022-11-28 19:09:44 -0300

    inicio de sesion mejora de front

--- src/Pantallas/Pantalla-principal.jsx
+++ src/Pantallas/Pantalla-principal.jsx
@@ -7,9 +7,9 @@ import { textAlign } from "@mui/system";
 function PantallaPrincipal() {
     return(
         <div className="PantallaPrincipal">
-            <div style={{minWidth:"60%", display:"flex", flexDirection:"column", padding:"20px", boxSizing:"border-box", }}>
-                <div style={{maxWidth:"80%", minHeight:"100%", display:"flex", flexDirection:"column", borderRadius:"10px"}} className="fondoGradiente">
-                    <div style={{minHeight:"20%", minWidth:"100%", boxSizing:"border-box", display:"flex", justifyContent:"center", alignItems:"center", marginLeft:"15px"}}>
+           <div style={{maxWidth:"60%", display:"flex", flexDirection:"column", padding:"20px", boxSizing:"border-box", }}>
+                <div style={{maxWidth:"90%", minHeight:"100%", display:"flex", flexDirection:"column", borderRadius:"10px"}} className="fondoGradiente">
+                    <div style={{minHeight:"15%", minWidth:"100%", boxSizing:"border-box", display:"flex", justifyContent:"center", alignItems:"center", marginLeft:"15px"}}>
                         <h1 style={{color:"#3D56B2"}}>Academy</h1>
                     </div>
                     <h5 style={{color:"#3D56B2", marginLeft:"20px"}}> Academy es una página para gestionar la contratación de clases, pensada para facilitar el contacto entre alumnos y profesores.</h5>
@@ -23,12 +23,12 @@ function PantallaPrincipal() {
                     <br></br>
                     <br></br>
                     <h5 style={{color:"#3D56B2", marginLeft:"15px" ,textAlign:"center"}}>Comience a explorar Academy:</h5>
-                    <div style={{marginTop: "-630px", minHeight:"80%", minWidth:"100%", boxSizing:"border-box", display:"flex", flexDirection:"column", justifyContent:"end", alignItems:"center"}}>
-                        <div style={{minWidth:"60%", boxSizing:"border-box", display:"flex", justifyContent:"center"}}>
-                            <Link to="/inicioSesion" style={{textDecoration:"none"}}><Button variant="outlined" size="large" style={{margin:"20px", color:"white", borderColor:"white", background:"#3D56B2"}} disableElevation>Iniciar sesion</Button></Link>
+                    <div style={{marginTop: "0px", minWidth:"100%", boxSizing:"border-box", display:"flex", flexDirection:"column", justifyContent:"", alignItems:"center"}}>
+                        <div style={{minWidth:"60%", boxSizing:"border-box", display:"flex", justifyContent:"center", maxHeight:"50%"}}>
+                            <Link to="/inicioSesion" style={{textDecoration:"none"}}><Button variant="outlined" size="large" style={{margin:"0px", color:"white", borderColor:"white", background:"#3D56B2"}} disableElevation>Iniciar sesion</Button></Link>
                         </div>
-                        <div style={{minWidth:"60%", boxSizing:"border-box", display:"flex", justifyContent:"center"}}>
-                            <Link to="/registro" style={{textDecoration:"none"}}><Button variant="outlined" size="large" style={{margin:"20px", color:"white", borderColor:"white", background:"#3D56B2"}}>Registrarse</Button></Link>
+                        <div style={{minWidth:"60%", boxSizing:"border-box", display:"flex", justifyContent:"center", maxHeight:"50%"}}>
+                            <Link to="/registro" style={{textDecoration:"none"}}><Button variant="outlined" size="large" style={{margin:"0px", color:"white", borderColor:"white", background:"#3D56B2"}}>Registrarse</Button></Link>
                         </div>
                     </div>
                 </div>
